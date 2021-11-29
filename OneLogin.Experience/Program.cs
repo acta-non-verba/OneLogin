afcebf9261efe5c9fd54147b3e5b048a3f5b8891
acta-non-verba <vikas.joshi@live.com>
2021-11-29 16:24:49 +0530

    Initial Commit

---
+++ OneLogin.Experience/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using OneLogin.Repository;
+namespace OneLogin.Experience
+{
+    class Program
+    {
+        public enum SSOMethod
+        {
+            CompanySSO = 7,
+            Google = 1,
+            Facebook = 2,
+            Twitter = 3,
+            LinkedIn = 4,
+            Aadhar = 5,
+            Office365 = 6
+        }
+        static void Main(string[] args)
+        {
+            int loginMethod = AskUserLoginPreference();
+            LoginRepository repository = new LoginRepository();
+            string authenticationStatus = "";
+            switch (loginMethod)
+            {
+                case 1:
+                    authenticationStatus = repository.AuthenticateGoogleUser();
+                    break;
+                case 2:
+                    authenticationStatus = repository.AuthenticateFacebookUser();
+                    break;
+                case 3:
+                    authenticationStatus = repository.AuthenticateTwitterUser();
+                    break;
+                case 4:
+                    authenticationStatus = repository.AuthenticateLinkedInUser();
+                    break;
+                case 5:
+                    authenticationStatus = repository.AuthenticateUsingAdharCard();
+                    break;
+                case 6:
+                    authenticationStatus = repository.AuthenticateOffice365User();
+                    break;
+                case 7:
+                    authenticationStatus = repository.AuthenticateUsingCompanyEnterpriseSSO();
+                    break;
+                default:
+                    authenticationStatus = repository.AuthenticateUsingCompanyEnterpriseSSO();
+                    break;
+            }
+            Console.WriteLine(authenticationStatus);
+        }
+        private static int AskUserLoginPreference()
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine("Login using " + i + "." + (SSOMethod)i);
+            }
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ OneLogin.Repository/LoginRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OneLogin.Repository
+{
+    public interface ILoginRepository
+    {
+        string AuthenticateFacebookUser();
+        string AuthenticateGoogleUser();
+        string AuthenticateLinkedInUser();
+        string AuthenticateOffice365User();
+        string AuthenticateTwitterUser();
+        string AuthenticateUsingAdharCard();
+        string AuthenticateUsingCompanyEnterpriseSSO();
+    }
+
+    public class LoginRepository : ILoginRepository
+    {
+        public LoginRepository()
+        {
+        }
+
+        public string AuthenticateUsingCompanyEnterpriseSSO()
+        {
+            return "company sso authentication status";
+        }
+        public string AuthenticateGoogleUser()
+        {
+            return "google authentication status";
+        }
+        public string AuthenticateFacebookUser()
+        {
+            return "Facebook user authentication status";
+        }
+        public string AuthenticateTwitterUser()
+        {
+            return "Twitter user authentication status";
+        }
+        public string AuthenticateLinkedInUser()
+        {
+            return "LikedIn user authentication status";
+        }
+        public string AuthenticateUsingAdharCard()
+        {
+            return "Aadhar authentication status";
+        }
+        public string AuthenticateOffice365User()
+        {
+            return "Office 365 authentication status";
+        }
+
+    }
+}
